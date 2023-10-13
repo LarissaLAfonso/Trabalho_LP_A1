da985906f82ff0f8d21b408bf3952274cce71ef1
Larissa Lemos Afonso <126449248+LarissaLAfonso@users.noreply.github.com>
2023-10-13 20:53:06 -0300

    change some registers in df_non_attendance

--- createobjects.py
+++ createobjects.py
@@ -1,6 +1,11 @@
+"""
+Contains functions that load the Excel file and create the pd.DataFrame objects used in the project.
+"""
+
 import pandas as pd
 import openpyxl
 import zipfile
+
 # openpyxl is used by pandas when calling the `read_excel` function. It was
 # imported here so the script can ensure the user have it installed before
 # trying to run anything.
@@ -27,14 +32,14 @@ def load_data_as_df(file_path:str) -> pd.DataFrame:
         quit()
     return dataframe
 
-df = load_data_as_df(r'Trabalho_LP_A1/resultados_cpc_2021.xlsx')
+df = load_data_as_df(r'Trabalho_LP_A1/dataframes/resultados_cpc_2021.xlsx')
 
-def create_non_attendance_df(df):
+def create_non_attendance_df(df:pd.DataFrame) -> pd.DataFrame:
     """
-    Create a dataframe with the non attendance mean of each course
+    Creates a dataframe with the non attendance mean of each course
 
     Parameters
-    ----------
+    ----------__
     df: pd.DataFrame
         Dataframe used for manipulation.
         
@@ -43,26 +48,47 @@ def create_non_attendance_df(df):
     pd.DataFrame
         A pandas dataframe with the data of the average non attendance rate for each course
     """
-    #Create new dataframe with only the columns we need
+    # Create new dataframe with only the columns we need
     df_1 = df[['Área de Avaliação', ' Nº de Concluintes Inscritos', ' Nº de Concluintes Participantes']]
 
-    #Drop missing values
+    # Drop missing values
     df_1["Área de Avaliação"].dropna(inplace=True)
 
-    #Remove everything after '(', leaving only the course name
+    # Remove everything after '(', leaving only the course name
     df_1['Área de Avaliação'] = df_1['Área de Avaliação'].apply(lambda x: x.split('(')[0].strip() if isinstance(x, str) else x)
 
-    #Create new column with the ration between the difference between enrolled and participants divided by enrolled
+    df_1["Área de Avaliação"] = df_1["Área de Avaliação"].str.title()
+
+    # Create new column with the ration between the difference between enrolled and participants divided by enrolled
     df_1["Taxa de Desistência"] = (df_1[" Nº de Concluintes Inscritos"] - df_1[" Nº de Concluintes Participantes"])/df_1[" Nº de Concluintes Inscritos"]
 
-    #Get the mean in "Taxa de Desistência" for each course
+    # Get the mean in "Taxa de Desistência" for each course
     grouped = df_1.groupby('Área de Avaliação')['Taxa de Desistência'].mean().reset_index()
 
-    #Merge the two dataframes
+    # Merge the two dataframes
     df_1 = df_1.merge(grouped, on='Área de Avaliação', suffixes=('', ' Média'))
 
-    #Rename the columns
+    # Rename the columns
     df_1.rename(columns={'D_Mean': 'Mean'}, inplace=True)
+
+    # Get final dataframe with the non attendance mean of each course
+    df_desist = df_1.groupby('Área de Avaliação')['Taxa de Desistência Média'].first().reset_index()
+    df_desist.sort_values(by=['Taxa de Desistência Média'], inplace=True, ascending=False)
+    
+    dictionary = {
+        "Tecnologia Em Redes De Computadores": "Redes De Computadores",
+        "Tecnologia Em Análise E Desenvolvimento De Sistemas": "Desenvolvimento De Sistemas",
+        "Tecnologia Em Gestão Da Tecnologia Da Informação": "Gestão De T.I."
+    }
+
+    # Change names that are too long
+    df_desist["Área de Avaliação"] = df_desist["Área de Avaliação"].apply(lambda x: dictionary[x] if x in dictionary else x)
+    
+    return df_desist
+
+# Dataframe used to plot the course x non attendance rate graph
+df_non_attendance = create_non_attendance_df(df)
+
 
     #Get final dataframe with the non attendance mean of each course
     df_desist = df_1.groupby('Área de Avaliação')['Taxa de Desistência Média'].first().reset_index()
