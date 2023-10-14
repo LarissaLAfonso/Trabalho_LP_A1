52e3528b178fe75e8455194bc070a7d9169a8a4b
vito0182 <vitor.mnw@gmail.com>
2023-10-14 15:51:29 -0300

    update main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import createobjects as co
 import data_cleaner as dc
+import graphs
 
-path = "./dataframes/resultados_cpc_2021.xlsx"
+data_path = "./dataframes/resultados_cpc_2021.csv"
 
-messy_df = co.load_data_as_df(path)
+messy_df = co.load_data_as_df(data_path)
+clean_df = dc.dataframe_cleaner(messy_df)
 
-df = dc.dataframe_cleaner(messy_df)
+non_attendence_df = co.create_non_attendance_df(clean_df)
+average_scores_df = co.create_average_nota_by_region(clean_df)
 
-print(df)
+
+non_attendence_graph = graphs.create_graph_non_attendance(non_attendence_df)
+average_scores_graph = graphs.create_graph_average_scores_by_region(average_scores_df)
+
+
+average_scores_graph.savefig('average_scores.png', dpi=300, bbox_inches='tight')
+non_attendence_graph.savefig('non_attendence.png', dpi=300, bbox_inches='tight')
