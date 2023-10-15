5b0a17c35fa068b31ef3f7c1cc2c6b913da6c85f
pedrotokar <pedrotokar2004@gmail.com>
2023-10-15 01:44:42 -0300

    Add basic files of documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'A1 - Linguagens de Programação'
+copyright = '2023, Larissa Lemos, Pedro Tokar, Vitor Nascimento'
+author = 'Larissa Lemos, Pedro Tokar, Vitor Nascimento'
+release = '2023'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
