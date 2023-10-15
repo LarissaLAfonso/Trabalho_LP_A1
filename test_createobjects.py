64bdfb30ed00b10de0fff149fff6dc90965c8ef2
vito0182 <vitor.mnw@gmail.com>
2023-10-15 19:14:35 -0300

    created more tests

--- test_createobjects.py
+++ test_createobjects.py
@@ -92,5 +92,40 @@ class TestCreateNonAttendanceDf(unittest.TestCase):
         self.assertTrue(len(result) > 0)
 
 
+class TestCreateRegionColumnDf(unittest.TestCase):
+
+    # Test if the function provide the correct outputs
+    def test_region_column_added_to_dataframe(self):
+
+        sample_data = pd.DataFrame({
+            'UF': ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
+        })
+
+        expected_region_list = ['Norte', 'Nordeste', 'Norte', 'Norte', 'Nordeste', 'Nordeste', 'Centro-Oeste', 'Sudeste', 'Centro-Oeste', 'Nordeste', 'Centro-Oeste', 'Centro-Oeste', 'Sudeste', 'Norte', 'Nordeste', 'Sul', 'Nordeste', 'Nordeste', 'Sudeste', 'Nordeste', 'Sul', 'Norte', 'Norte', 'Sul', 'Sudeste', 'Nordeste', 'Norte']
+
+        region_series = create_region_column_df(sample_data, 'UF')
+
+        region_list = region_series.to_list()
+
+        # check if the 'Region' column is present in the result
+        self.assertEqual(region_list, expected_region_list)
+
+
+class TestCreateAverageNotaByRegion(unittest.TestCase):
+
+    # Test if the function raises a KeyError when the input DataFrame doesn't have all needed columns
+    def test_raises_key_error_when_missing_columns(self):
+
+        sample_data = pd.DataFrame({
+            'Sigla da UF ': ['SP', 'RJ', 'MG', 'BA', 'AM', 'PA'],
+            ' Nota Padronizada - Organização Didático-Pedagógica': [8.5, 7.2, 6.8, 8.0, 7.5, 6.2],
+            ' Nota Padronizada - Infraestrutura e Instalações Físicas': [7.8, 7.5, 6.4, 8.2, 6.9, 6.1],
+            ' Nota Padronizada - Oportunidade de Ampliação da Formação': [7.2, 7.0, 6.3, 7.9, 6.5, 5.9]
+        })
+
+        # quits the program when the dataframe passed is missing a column
+        with self.assertRaises(SystemExit):
+            create_average_nota_by_region(sample_data)
+            
 if __name__ == '__main__':
     unittest.main()
