71efdbd3763a94b6e121b0e917053fe2d036f913
vito0182 <vitor.mnw@gmail.com>
2023-10-15 18:39:19 -0300

    add function that tests Load Data as Geodf function

--- test_createobjects.py
+++ test_createobjects.py
@@ -1,6 +1,8 @@
 
 from createobjects import load_data_as_df
+from createobjects import load_data_as_geodf
 import pandas as pd
+import geopandas as gpd
 
 import unittest
 
@@ -31,6 +33,29 @@ class TestLoadDataAsDf(unittest.TestCase):
         file_path = "./data/dataframes/nonexistent_file.xlsx"
         with self.assertRaises(SystemExit):
             load_data_as_df(file_path)
+
+
+class TestLoadDataAsGeodf(unittest.TestCase):
+
+    # loads data from a valid .json file and returns a geopandas geodataframe
+    def test_valid_json_file(self):
+        file_path = "./data/map/brasil_estados.json"
+        geodf = load_data_as_geodf(file_path)
+        self.assertIsInstance(geodf, gpd.GeoDataFrame)
+
+    # loads data from a .json file with multiple features and returns a geopandas geodataframe with multiple rows
+    def test_multiple_features(self):
+        file_path = "./data/map/brasil_estados.json"
+        geodf = load_data_as_geodf(file_path)
+        self.assertGreater(len(geodf), 1)
+
+    # quits the program when FileNotFoundError is raised
+    def test_quit_program_on_error(self):
+        file_path = "./data/dataframes/nonexistent_file.json"
+        with self.assertRaises(SystemExit):
+            load_data_as_df(file_path)
+
+
 
 if __name__ == '__main__':
     unittest.main()
